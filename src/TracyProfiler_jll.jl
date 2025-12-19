c11d3b286095f81f6f191d12e5514f06e5640e53
jlbuild <juliabuildbot@gmail.com>
2025-12-19 22:23:58 +0000

    TracyProfiler_jll build 0.13.1+0

--- src/TracyProfiler_jll.jl
+++ src/TracyProfiler_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("TracyProfiler")
-JLLWrappers.@generate_main_file("TracyProfiler", UUID("0c351ed6-8a68-550e-8b79-de6f926da83c"))
+JLLWrappers.@generate_main_file("TracyProfiler", Base.UUID("0c351ed6-8a68-550e-8b79-de6f926da83c"))
 end  # module TracyProfiler_jll
